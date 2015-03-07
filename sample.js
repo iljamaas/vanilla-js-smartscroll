0b995bcc9494af3711dcc170fa0108a4204f2f05
Ilja Maas <iljamaas@dreamsolution.nl>
2015-03-07 20:25:13 +0100

    Initial commit

---
+++ sample.js
@@ -0,0 +1,510 @@
+// Some callbacks first
+function filterer(item, search){
+    // Check if item should be shown (return true) or not (return false)
+    // 'search' can be anything.. depending on what you feed the list. See below.
+    var c = item.engine + ' ' + item.browser + ' ' + item.platform + ' ' + item.version + ' ' + item.grade;
+    return (c.toLowerCase().indexOf(search.toLowerCase()) != -1)
+}
+
+function renderer(item){
+    // Render a row. Please note; item is referenced back to it's source! So any modification
+    // here will be ... a modification :-)
+    // Most likely you will need some SANE formatter here. Mustache.js?
+    var c = item.engine + ' <b>' + item.browser + '</b> ' + item.platform + ' ' + item.version + ' ' + item.grade;
+    return '<div class="contentblock">' + item.id + ' :: ' + c + '</div>';
+}
+
+function styler(width, height) {
+    // A bit nicer function to set heights to the list. prevent having style="height: XXpx;" attributes throughout
+    // the code.
+    return '.list-container { height: $Hpx }'.replace('$H', '' + (height-100));
+}
+
+// Some variables you will probably implement in a meaningfull way..
+
+// A nice list with some data
+var data = [
+    {
+        id: 1,
+        engine: "Gecko",
+        browser: "Firefox 1.5",
+        platform: "Win 98+ / OSX.2+",
+        version: "1.8",
+        grade: "A"
+    },
+    {
+        id: 2,
+        engine: "Gecko",
+        browser: "Firefox 2.0",
+        platform: "Win 98+ / OSX.2+",
+        version: "1.8",
+        grade: "A"
+    },
+    {
+        id: 3,
+        engine: "Gecko",
+        browser: "Firefox 3.0",
+        platform: "Win 2k+ / OSX.3+",
+        version: "1.9",
+        grade: "A"
+    },
+    {
+        id: 4,
+        engine: "Gecko",
+        browser: "Camino 1.0",
+        platform: "OSX.2+",
+        version: "1.8",
+        grade: "A"
+    },
+    {
+        id: 5,
+        engine: "Gecko",
+        browser: "Camino 1.5",
+        platform: "OSX.3+",
+        version: "1.8",
+        grade: "A"
+    },
+    {
+        id: 6,
+        engine: "Gecko",
+        browser: "Netscape 7.2",
+        platform: "Win 95+ / Mac OS 8.6-9.2",
+        version: "1.7",
+        grade: "A"
+    },
+    {
+        id: 7,
+        engine: "Gecko",
+        browser: "Netscape Browser 8",
+        platform: "Win 98SE+",
+        version: "1.7",
+        grade: "A"
+    },
+    {
+        id: 8,
+        engine: "Gecko",
+        browser: "Netscape Navigator 9",
+        platform: "Win 98+ / OSX.2+",
+        version: "1.8",
+        grade: "A"
+    },
+    {
+        id: 9,
+        engine: "Gecko",
+        browser: "Mozilla 1.0",
+        platform: "Win 95+ / OSX.1+",
+        version: "1",
+        grade: "A"
+    },
+    {
+        id: 10,
+        engine: "Gecko",
+        browser: "Mozilla 1.1",
+        platform: "Win 95+ / OSX.1+",
+        version: "1.1",
+        grade: "A"
+    },
+    {
+        id: 11,
+        engine: "Gecko",
+        browser: "Mozilla 1.2",
+        platform: "Win 95+ / OSX.1+",
+        version: "1.2",
+        grade: "A"
+    },
+    {
+        id: 12,
+        engine: "Gecko",
+        browser: "Mozilla 1.3",
+        platform: "Win 95+ / OSX.1+",
+        version: "1.3",
+        grade: "A"
+    },
+    {
+        id: 13,
+        engine: "Gecko",
+        browser: "Mozilla 1.4",
+        platform: "Win 95+ / OSX.1+",
+        version: "1.4",
+        grade: "A"
+    },
+    {
+        id: 14,
+        engine: "Gecko",
+        browser: "Mozilla 1.5",
+        platform: "Win 95+ / OSX.1+",
+        version: "1.5",
+        grade: "A"
+    },
+    {
+        id: 15,
+        engine: "Gecko",
+        browser: "Mozilla 1.6",
+        platform: "Win 95+ / OSX.1+",
+        version: "1.6",
+        grade: "A"
+    },
+    {
+        id: 16,
+        engine: "Gecko",
+        browser: "Mozilla 1.7",
+        platform: "Win 98+ / OSX.1+",
+        version: "1.7",
+        grade: "A"
+    },
+    {
+        id: 17,
+        engine: "Gecko",
+        browser: "Mozilla 1.8",
+        platform: "Win 98+ / OSX.1+",
+        version: "1.8",
+        grade: "A"
+    },
+    {
+        id: 18,
+        engine: "Gecko",
+        browser: "Seamonkey 1.1",
+        platform: "Win 98+ / OSX.2+",
+        version: "1.8",
+        grade: "A"
+    },
+    {
+        id: 19,
+        engine: "Gecko",
+        browser: "Epiphany 2.20",
+        platform: "Gnome",
+        version: "1.8",
+        grade: "A"
+    },
+    {
+        id: 20,
+        engine: "Gecko (UTF-8: $¢€ היצוא)",
+        browser: "Firefox 1.0",
+        platform: "Win 98+ / OSX.2+",
+        version: "1.7",
+        grade: "A"
+    },
+    {
+        id: 21,
+        engine: "KHTML",
+        browser: "Konqureror 3.1",
+        platform: "KDE 3.1",
+        version: "3.1",
+        grade: "C"
+    },
+    {
+        id: 22,
+        engine: "KHTML",
+        browser: "Konqureror 3.3",
+        platform: "KDE 3.3",
+        version: "3.3",
+        grade: "A"
+    },
+    {
+        id: 23,
+        engine: "KHTML",
+        browser: "Konqureror 3.5",
+        platform: "KDE 3.5",
+        version: "3.5",
+        grade: "A"
+    },
+    {
+        id: 24,
+        engine: "Misc",
+        browser: "NetFront 3.1",
+        platform: "Embedded devices",
+        version: "-",
+        grade: "C"
+    },
+    {
+        id: 25,
+        engine: "Misc",
+        browser: "NetFront 3.4",
+        platform: "Embedded devices",
+        version: "-",
+        grade: "A"
+    },
+    {
+        id: 26,
+        engine: "Misc",
+        browser: "Dillo 0.8",
+        platform: "Embedded devices",
+        version: "-",
+        grade: "X"
+    },
+    {
+        id: 27,
+        engine: "Misc",
+        browser: "Links",
+        platform: "Text only",
+        version: "-",
+        grade: "X"
+    },
+    {
+        id: 28,
+        engine: "Misc",
+        browser: "Lynx",
+        platform: "Text only",
+        version: "-",
+        grade: "X"
+    },
+    {
+        id: 29,
+        engine: "Misc",
+        browser: "IE Mobile",
+        platform: "Windows Mobile 6",
+        version: "-",
+        grade: "C"
+    },
+    {
+        id: 30,
+        engine: "Misc",
+        browser: "PSP browser",
+        platform: "PSP",
+        version: "-",
+        grade: "C"
+    },
+    {
+        id: 31,
+        engine: "Other browsers",
+        browser: "All others",
+        platform: "-",
+        version: "-",
+        grade: "U"
+    },
+    {
+        id: 32,
+        engine: "Presto",
+        browser: "Opera 7.0",
+        platform: "Win 95+ / OSX.1+",
+        version: "-",
+        grade: "A"
+    },
+    {
+        id: 33,
+        engine: "Presto",
+        browser: "Opera 7.5",
+        platform: "Win 95+ / OSX.2+",
+        version: "-",
+        grade: "A"
+    },
+    {
+        id: 34,
+        engine: "Presto",
+        browser: "Opera 8.0",
+        platform: "Win 95+ / OSX.2+",
+        version: "-",
+        grade: "A"
+    },
+    {
+        id: 35,
+        engine: "Presto",
+        browser: "Opera 8.5",
+        platform: "Win 95+ / OSX.2+",
+        version: "-",
+        grade: "A"
+    },
+    {
+        id: 36,
+        engine: "Presto",
+        browser: "Opera 9.0",
+        platform: "Win 95+ / OSX.3+",
+        version: "-",
+        grade: "A"
+    },
+    {
+        id: 37,
+        engine: "Presto",
+        browser: "Opera 9.2",
+        platform: "Win 88+ / OSX.3+",
+        version: "-",
+        grade: "A"
+    },
+    {
+        id: 38,
+        engine: "Presto",
+        browser: "Opera 9.5",
+        platform: "Win 88+ / OSX.3+",
+        version: "-",
+        grade: "A"
+    },
+    {
+        id: 39,
+        engine: "Presto",
+        browser: "Opera for Wii",
+        platform: "Wii",
+        version: "-",
+        grade: "A"
+    },
+    {
+        id: 40,
+        engine: "Presto",
+        browser: "Nokia N800",
+        platform: "N800",
+        version: "-",
+        grade: "A"
+    },
+    {
+        id: 41,
+        engine: "Presto",
+        browser: "Nintendo DS browser",
+        platform: "Nintendo DS",
+        version: "8.5",
+        grade: "C/A1"
+    },
+    {
+        id: 42,
+        engine: "Tasman",
+        browser: "Internet Explorer 4.5",
+        platform: "Mac OS 8-9",
+        version: "-",
+        grade: "X"
+    },
+    {
+        id: 43,
+        engine: "Tasman",
+        browser: "Internet Explorer 5.1",
+        platform: "Mac OS 7.6-9",
+        version: "1",
+        grade: "C"
+    },
+    {
+        id: 44,
+        engine: "Tasman",
+        browser: "Internet Explorer 5.2",
+        platform: "Mac OS 8-X",
+        version: "1",
+        grade: "C"
+    },
+    {
+        id: 45,
+        engine: "Trident",
+        browser: "Internet Explorer 4.0",
+        platform: "Win 95+ (Entity: &)",
+        version: "4",
+        grade: "X"
+    },
+    {
+        id: 46,
+        engine: "Trident",
+        browser: "Internet Explorer 5.0",
+        platform: "Win 95+",
+        version: "5",
+        grade: "C"
+    },
+    {
+        id: 47,
+        engine: "Trident",
+        browser: "Internet Explorer 5.5",
+        platform: "Win 95+",
+        version: "5.5",
+        grade: "A"
+    },
+    {
+        id: 48,
+        engine: "Trident",
+        browser: "Internet Explorer 6",
+        platform: "Win 98+",
+        version: "6",
+        grade: "A"
+    },
+    {
+        id: 49,
+        engine: "Trident",
+        browser: "Internet Explorer 7",
+        platform: "Win XP SP2+",
+        version: "7",
+        grade: "A"
+    },
+    {
+        id: 50,
+        engine: "Trident",
+        browser: "AOL browser (AOL desktop)",
+        platform: "Win XP",
+        version: "6",
+        grade: "A"
+    },
+    {
+        id: 51,
+        engine: "Webkit",
+        browser: "Safari 1.2",
+        platform: "OSX.3",
+        version: "125.5",
+        grade: "A"
+    },
+    {
+        id: 52,
+        engine: "Webkit",
+        browser: "Safari 1.3",
+        platform: "OSX.3",
+        version: "312.8",
+        grade: "A"
+    },
+    {
+        id: 53,
+        engine: "Webkit",
+        browser: "Safari 2.0",
+        platform: "OSX.4+",
+        version: "419.3",
+        grade: "A"
+    },
+    {
+        id: 54,
+        engine: "Webkit",
+        browser: "Safari 3.0",
+        platform: "OSX.4+",
+        version: "522.1",
+        grade: "A"
+    },
+    {
+        id: 55,
+        engine: "Webkit",
+        browser: "OmniWeb 5.5",
+        platform: "OSX.4+",
+        version: "420",
+        grade: "A"
+    },
+    {
+        id: 56,
+        engine: "Webkit",
+        browser: "iPod Touch / iPhone",
+        platform: "iPod",
+        version: "420.1",
+        grade: "A"
+    },
+    {
+        id: 57,
+        engine: "Webkit",
+        browser: "S60",
+        platform: "S60",
+        version: "413",
+        grade: "A"
+    }
+]
+
+// Searchterm could be some text, but also a more complex structure, it is passed to the filterfn callback
+// function, in this example the filterer function above.
+var search = '';
+
+// Order can be simply one of the data 'keys'  e.g. 'id' with possible '-' as prefix for descending sort.
+// You can assign a custum sorter-function to 'order' to use more complex sorting algorithms.
+var order = '-id';
+
+
+// Actual use of the smartscoll
+
+// Sane styling
+smartscroll.viewport_styler.addStyleCreator(styler);
+
+// Instantiate a list
+var myList = smartscroll.List({renderfn: renderer, filterfn: filterer});
+// Feed the list with some data
+myList.loadData(data);
+// Initialise the list
+myList.set({filter: search, order: order}); // the set function MUST at least be called once!
+
+// Instantiate the smartscroll by assign a container and the list
+myPane = smartscroll.Scroll( {container: document.querySelector('.list-container'), list: myList } );
+myList.addListener(myPane);  // Usefull if you tend to CHANGE the data
+
+myPane.setRowHeight(100); // 100 is also the default, but just as an example
---
+++ scroller.src.js
@@ -0,0 +1,358 @@
+/**
+ *
+ * Smartscroll
+ *
+ * 2014 Dreamsolution B.V., Delft, The Netherlands
+ *
+ */
+smartscroll = function() {
+    'use strict';
+
+    function createStyler(window) {
+        var _handle, // timeout handle
+            _stylers, // style create functions
+            _listeners; // resize listeners
+
+        (function() {
+            _listeners = [];
+            _stylers = [];
+            window.addEventListener('resize', onResize, false);
+            window.addEventListener('unload', onUnload, false);
+        })();
+
+        function create_styler(styleFn) {
+            return function(width, height) {
+                var s = '\n';
+                if ('name' in styleFn)
+                    s += '/* ' + styleFn.name + ' */\n';
+                s += styleFn(width, height);
+                return s;
+            };
+        }
+
+        // Create a new style element with the given CSS and append, and remove
+        // any earlier style element _after_ that.
+        function replace_style_elm(text) {
+            var cstyler = document.querySelector('head .viewport-styler');
+
+            document.querySelector('head').insertAdjacentHTML( 'beforeend',
+                    '<style type="text/css" class="viewport-styler">' +
+                    text + '</style>');
+
+            if (cstyler){
+                cstyler.parentNode.removeChild(cstyler);
+            }
+        }
+
+        function eval_stylers() {
+            var i,
+                exprs = [],
+                width = window.innerWidth,
+                height = window.innerHeight;
+
+            _handle = undefined;
+            for (i = 0; i < _stylers.length; i++) {
+                exprs.push(_stylers[i](width, height));
+            }
+            replace_style_elm(exprs.join('\n'));
+
+            for (i = _listeners.length; i--; ) {
+                _listeners[i]();
+            }
+        }
+
+        function onResize() {
+            if (_handle)
+                window.clearTimeout(_handle);
+            _handle = window.setTimeout(eval_stylers, 120);
+        }
+
+        function onUnload() {
+            window.clearTimeout(_handle);
+            _listeners = _handle = _stylers = undefined;
+        }
+
+        return {
+            resize: function resize(callbackFn) {
+                _listeners.push(callbackFn);
+            },
+            addStyleCreator: function addStyleCreator(styleFn) {
+                var styler = create_styler(styleFn),
+                    width = window.innerWidth,
+                    height = window.innerHeight;
+
+                _stylers[_stylers.length] = styler;
+                // Immediately append the style:
+                var elm = document.querySelector('head .viewport-styler');
+                var text = elm ? elm.innerHTML : '';
+                replace_style_elm(text + styler(width, height));
+            },
+            redraw: function redraw() {
+                return onResize();
+            }
+        };
+    }
+
+
+    function smartScroll(conf) {
+        var wrapper,
+            pane,
+            actualcontent,
+            heightforcer,
+            smartlist;
+
+        var rowHeight = 120,
+            listlength = 0,
+            curfirst = 1,
+            drawnum = 0;
+
+        (function() {
+            if (conf.hasOwnProperty('height')) {
+                rowHeight = conf.height;
+            }
+            smartlist = conf.list;
+            wrapper = conf.container
+
+            var structure = '<div class="smsc_sp"><div class="smsc_faker"></div><div class="smsc_ac"></div></div>';
+            wrapper.insertAdjacentHTML('afterbegin', structure);
+            wrapper.classList.add('smsc_wrap');
+
+
+            heightforcer = wrapper.querySelector('.smsc_faker');
+            pane = wrapper.querySelector('.smsc_sp');
+            actualcontent = wrapper.querySelector('.smsc_ac');
+
+            pane.style.height = window.getComputedStyle(wrapper).height;
+            pane.addEventListener('scroll', adjustView, false);
+
+            styler.resize(onStylerResize);
+
+            layout();
+        })();
+
+        function transform(d) {
+                actualcontent.style.transform = 'translate(0px, ' + d + 'px)';
+        }
+
+        function layout() {
+            listlength = smartlist.getSize();
+
+            drawnum = Math.ceil(parseInt(window.getComputedStyle(pane).height,10) / rowHeight) + 1;
+            heightforcer.style.height = (listlength * rowHeight) + 'px';
+
+            if(pane.scrollTop > listlength * rowHeight){
+                pane.scrollTop = listlength * rowHeight - parseInt(window.getComputedStyle(pane).height,10);
+            }
+            adjustView(null, true);
+        }
+
+        function adjustView(event, listchanged) {
+            var dx = 0;
+            var o = pane.scrollTop;
+            o = o > 0 ? o - 1 : 0;
+
+            var firstElement = Math.floor((o) / rowHeight) + 1;
+
+
+            if (drawnum < listlength && firstElement > listlength-drawnum + 1) {
+                firstElement = listlength - drawnum + 1;
+                dx = rowHeight;
+            }
+
+            if (curfirst !== firstElement || listchanged) {
+                drawContent(firstElement, listchanged);
+
+            }
+
+            transform(Math.min(o - ((o % rowHeight))) - dx);
+
+            curfirst = firstElement;
+        }
+
+        function drawContent(startWith, listchanged) {
+            /** if moving by one, just remove first one
+             * and append one or vice-verse to keep performance. prevent unneeded redraws.
+             */
+            var t;
+
+            if (!listchanged && startWith == curfirst + 1) {
+                actualcontent.removeChild(actualcontent.firstChild); // children('div:first-child').remove();
+                actualcontent.insertAdjacentHTML('beforeend',smartlist.renderItemAtPosition(curfirst + drawnum));
+            }
+            else if (!listchanged && startWith == curfirst) {
+                actualcontent.removeChild(actualcontent.lastChild);
+                actualcontent.insertAdjacentHTML('afterbegin',smartlist.renderItemAtPosition(curfirst -1));
+            }
+            else {
+                // too much of a hassle.. just redraw!
+                actualcontent.innerHTML = '';
+                for (var i = startWith; i < startWith + drawnum && i - startWith < listlength; i++) {
+                    t = smartlist.renderItemAtPosition(i);
+                    if (t) {
+                        actualcontent.insertAdjacentHTML('beforeend',t);
+                    }
+                }
+            }
+        }
+
+
+        function onStylerResize() {
+            pane.style.height = window.getComputedStyle(wrapper).height;
+            layout();
+        }
+
+        return {
+            handleEvent: function handleEvent(type, msg) {
+                if (type == 'listchanged') {
+                    layout();
+                    var event = new CustomEvent('my-event', {detail: msg});
+                    wrapper.dispatchEvent(event);
+                }
+            },
+            setRowHeight: function setRowHeight(h) {
+                // must be called if the row-detail-level is changed!
+                rowHeight = h;
+                layout();
+            }
+        };
+    }
+
+
+    function smartList(conf) {
+        var list = [],
+            shadowlist = [], // contains filtered or ordered list
+            listeners = [],
+            filterfn = conf.filterfn,
+            renderfn = conf.renderfn,
+            order = "id",
+            filter = "";
+
+
+        // We're using an object-based event handling, and not callback so that
+        // we can easily hook up a smartscroll object to the accompanying
+        // smartlist object.
+        function fireEvent(type, evt) {
+            for (var i = listeners.length; i--; ) {
+                listeners[i].handleEvent.apply(listeners[i], [type, evt]);
+            }
+        }
+
+        // filter callback implementation for browsers that
+        // support js 1.6 array.filter:
+        function js_filter_callback(item) {
+            return filterfn(item, filter);
+        }
+
+        function maintainShadowList() {
+            if ('filter' in list && typeof list.filter == 'function') {
+                shadowlist = list.filter(js_filter_callback);
+            } else {
+                shadowlist = [];
+                for (var i = 0; i < list.length; i++)
+                    if (filterfn(list[i], filter))
+                        shadowlist[shadowlist.length] = list[i];
+            }
+            shadowlist.sort(dynamicSort());
+            fireEvent('listchanged', {
+                size_total: list.length,
+                size_filtered: shadowlist.length
+            });
+        }
+
+        function dynamicSort() {
+            if (typeof order == 'function') {
+                return order;
+            } else {
+                var ordering = order[0] == "-" ? -1 : 1,
+                    prop = ordering == -1 ? order.substr(1) : order;
+
+                return function sorter(a, b) {
+                    // XXX IM: Force the null values to be on the end,
+                    // always...(regardless of sort order):
+                    //
+                    var prop_a = a[prop],
+                        prop_b = b[prop],
+                        missing_in_a = prop_a === undefined || prop_a === null,
+                        missing_in_b = prop_b === undefined || prop_b === null;
+
+                    if (missing_in_a && missing_in_b) {
+                        return 0;
+                    } else if (missing_in_a) {
+                        return 1;
+                    } else if (missing_in_b) {
+                        return -1;
+                    } else {
+                        return ordering * ((prop_a < prop_b) ?
+                            -1 : (prop_a > prop_b) ? 1 : 0);
+                    }
+                };
+            }
+        }
+
+        return {
+            loadData: function loadData(data, redraw) {
+                list = data;
+                if (redraw) {
+                    maintainShadowList();
+                }
+            },
+
+            set: function set(settings) {
+                var mt = false;
+                if (settings.hasOwnProperty('filter') && settings.filter !== filter) {
+                    filter = settings.filter;
+                    mt = true;
+                }
+                if (settings.hasOwnProperty('order') && settings.order !== order) {
+                    order = settings.order;
+                    mt = true;
+                }
+                if (mt) {
+                    maintainShadowList();
+                }
+            },
+
+            getSize: function getSize() {
+                return shadowlist.length;
+            },
+
+            renderItemAtPosition: function renderItemAtPosition(offset) {
+                if (offset >= 0 && offset <= shadowlist.length){
+                    return renderfn(shadowlist[offset - 1]);
+                } else {
+                    return '';
+                }
+            },
+
+            addListener: function addListener(listener) {
+                listeners.push(listener);
+            },
+
+            /**
+             * Sort function for use in the "order" config option.
+             */
+            lowerCompare: function lowerCompare(a, b) {
+                a = ("" + a).toLowerCase();
+                b = ("" + b).toLowerCase();
+                return a.localeCompare(b);
+            },
+
+            debug: function debug() {
+                var t = [];
+                for (var i = 0; i< shadowlist.length; i++) {
+                    t.push(shadowlist[i].id);
+                }
+            }
+        };
+    }
+
+
+    // There should be one instance of the styler per window.
+    var styler = createStyler(window);
+
+    return {
+        viewport_styler: styler,
+        Scroll: smartScroll,
+        List: smartList
+    };
+}();
